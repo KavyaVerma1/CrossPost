9dfc0d58601bf29df4c70ff780a989bdaf090450
KavyaVerma1 <12082003kavya@gmail.com>
2025-03-24 23:05:27 +0530

    sign up and sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,8 @@ import { FC } from 'react'
 import Link from 'next/link';
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/Button';
-import SignIn from '@/components/SignIn';
+import SignIn from '@/components/SignIn'
+import { ChevronLeft } from 'lucide-react'
     const page: FC = () => {
         return <div className='absolute inset-0'>
             <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
@@ -12,7 +13,7 @@ import SignIn from '@/components/SignIn';
                     buttonVariants({variant: 'ghost'}),
                      'self-start -mt-20'
                     )}>
-                    Home
+                   <ChevronLeft className='mr-2 h-4 w-4'/> Home
                 </Link> 
 
                 <SignIn/>
