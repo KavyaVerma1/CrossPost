9903eb3acd1812354f2aaad0d88b2863e7b56181
KavyaVerma1 <12082003kavya@gmail.com>
2025-03-25 11:43:30 +0530

    create community

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,44 @@
-import '@/styles/globals.css'
-import {Inter} from "next/font/google"
+import Navbar from '@/components/Navbar'
 import { cn } from '@/lib/utils'
-import Navbar from "@/components/Navbar"
-import {Toaster} from "@/components/ui/Toaster"
+import { Inter } from 'next/font/google'
+import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/Toaster'
+
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata = {
-  title: 'Crosspost',
+  title: 'CrossPost',
   description: 'A community platform built with Next.js and TypeScript.',
 }
 
-const inter=Inter({subsets:['latin']})
 export default function RootLayout({
   children,
-  authModal
+  authModal,
 }: {
   children: React.ReactNode
   authModal: React.ReactNode
 }) {
   return (
-    <html 
-    lang='en' 
-    className={cn('bg-white text-slate-900 antialiased light',
-    inter.className)}>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/*  @ts-expect-error server component*/ }
-        <Navbar/>
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          {authModal}
 
-        {authModal}
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-        {children}
-        </div>
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
+            {children}
+          </div>
+        </Providers>
         <Toaster />
-        </body>
+      </body>
     </html>
   )
 }
